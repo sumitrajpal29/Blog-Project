1385c2153c09f0a23cb2307c65d3ef7b3720b525
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2021-03-23 00:16:43 +0530

    Completed the Blog Project

---
+++ app.js
@@ -0,0 +1,64 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const postArray = [];
+
+app.get("/",function(req,res){
+  res.render("home",{CONTENT:homeStartingContent,POSTARRAY:postArray});
+  postArray.forEach(function(post){
+    console.log(post);
+  })
+})
+
+app.get("/posts/:hello",function(req,res){
+  const hello = lodash.lowerCase(req.params.hello);
+
+  postArray.forEach(function(post){
+    if(lodash.lowerCase(req.params.hello)===lodash.lowerCase(post.title)){
+    console.log("Test found! : "+hello);
+    res.render("post",{HEADING:post.title,CONTENT:post.post})
+  }
+  else
+    console.log("Not found!");
+  });
+})
+
+app.get("/about",function(req,res){
+  res.render("about",{CONTENT:aboutContent})
+})
+
+app.get("/contact",function(req,res){
+  res.render("contact",{CONTENT:contactContent})
+})
+
+app.get("/compose",function(req,res){
+  res.render("compose")
+})
+
+app.post("/compose",function(req,res){
+  // const post = {
+  //   heading:req.body.title,
+  //   post:req.body.post
+  // }
+  postArray.push(req.body);
+  res.redirect("/");
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
