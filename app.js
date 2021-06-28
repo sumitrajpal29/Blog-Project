9c2f483f230febdc3e272de37bd30cb4fc16a12b
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2021-06-28 20:29:47 +0530

    Added a MongoDB local db for this BlogPosts

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,27 +17,42 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const postArray = [];
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true });
+
+const postSchema = new mongoose.Schema({
+  title:{
+    type:String,
+    required:true},
+  content:{
+    type:String,
+    required:true}
+});
+const Post = mongoose.model("Post",postSchema);
 
 app.get("/",function(req,res){
-  res.render("home",{CONTENT:homeStartingContent,POSTARRAY:postArray});
-  postArray.forEach(function(post){
-    console.log(post);
+  Post.find({},function(err,found){
+    const posts = found;
+    res.render("home",{CONTENT:homeStartingContent,POSTARRAY:posts});
+    console.log(posts);
   })
-})
+});
 
 app.get("/posts/:hello",function(req,res){
-  const hello = lodash.lowerCase(req.params.hello);
-
-  postArray.forEach(function(post){
-    if(lodash.lowerCase(req.params.hello)===lodash.lowerCase(post.title)){
-    console.log("Test found! : "+hello);
-    res.render("post",{HEADING:post.title,CONTENT:post.post})
-  }
-  else
-    console.log("Not found!");
+  //const hello = lodash.lowerCase(req.params.hello);
+  const hello = req.params.hello;
+  console.log(hello);
+  Post.findOne({_id:hello},function(err,found){
+    if(!err){
+      if(found){
+        console.log("Found : "+found.title);
+        res.render("post",{HEADING:found.title,CONTENT:found.content})
+      }else{
+        console.log("Not found : "+hello+",No error!");
+      }
+    }
+    else console.log(err);
   });
-})
+});
 
 app.get("/about",function(req,res){
   res.render("about",{CONTENT:aboutContent})
@@ -51,13 +67,13 @@ app.get("/compose",function(req,res){
 })
 
 app.post("/compose",function(req,res){
-  // const post = {
-  //   heading:req.body.title,
-  //   post:req.body.post
-  // }
-  postArray.push(req.body);
-  res.redirect("/");
-})
+  const newPost = new Post({title:req.body.title,content:req.body.post});
+  newPost.save(function(err){
+    if(!err)
+    res.redirect("/");
+    else console.log(err)
+  })
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
